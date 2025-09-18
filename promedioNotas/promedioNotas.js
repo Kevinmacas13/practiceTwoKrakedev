a8ace3f2c3aaf19a4672b09c8fcec60e981bc177
kevin <kevinmacash@gmail.com>
2025-09-18 10:51:25 -0500

    implementa la función calcularPromedioNotas

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,11 @@
+calcularPromedioNotas = function () {
+  let n1;
+  let n2;
+  let n3;
+  let promedio;
+  n1 = recuperarFloat("txtNota1");
+  n2 = recuperarFloat("txtNota2");
+  n3 = recuperarFloat("txtNota3");
+  promedio = calcularPromedio(n1, n2, n3);
+  mostrarTexto("lblPromedio", "El promedio es: " + promedio.toFixed(2));
+};
---
+++ promedioNotas/serviciosPromedioNotas.js
@@ -0,0 +1,9 @@
+calcularPromedio=function(n1,n2,n3){    
+let suma;
+let division;
+let promedio;
+suma=n1+n2+n3;
+division=3;
+promedio=suma/division;
+return promedio;
+}
