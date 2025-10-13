3943475b9e600d90a31943faf35d03956ff4da4f
kevin <kevinmacash@gmail.com>
2025-10-13 10:23:09 -0500

    se completó el paso 0

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -1,1 +1,8 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function (letra) {
+  if (letra.charCodeAt(0) >= 65 && letra.charCodeAt(0) <= 90) {
+    return true;        
+  } else {
+    return false;
+  }
+};
