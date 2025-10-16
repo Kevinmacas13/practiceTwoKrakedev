8c5656dc7bd2d0f9cdb4b044c8d36f0c601ad7b3
kevin <kevinmacash@gmail.com>
2025-10-16 08:55:23 -0500

    se finaliza la parte 1 de la evaluación individual

---
+++ KevinMacas/orden.js
@@ -0,0 +1,35 @@
+let personas = [
+  { nombre: "Marcos", edad: 18 },
+  { nombre: "Roberto", edad: 15 },
+  { nombre: "Kate", edad: 25 },
+  { nombre: "Diana", edad: 12 },
+  { nombre: "Benja", edad: 5 },
+];
+
+agregarPersona = function () {
+  let nombrePersona;
+  let edadPersona;
+  let valido = false;
+
+  nombrePersona = recuperarTexto("txtNombre");
+  if (nombrePersona.length > 3) {
+    valido = true;
+  } else {
+    valido = false;
+    mostrarTexto("lblErrorNombre", "Debe tener al menos 3 caracteres");
+  }
+  edadPersona = recuperarInt("txtEdad");
+  if (edadPersona >= 0 && edadPersona <= 100) {
+    valido = true;
+  } else {
+    valido = false;
+    mostrarTexto("lblErrorEdad", "Debe estar entre 0 y 100");
+  }
+  if (valido) {
+    let persona = {};
+    persona.nombre = nombrePersona;
+    persona.edad = edadPersona;
+    personas.push(persona);
+    alert("PERSONA AGREGADA CORRECTAMENTE");
+  }
+};
